b7fe1824526ea1093d8f23463a3790ed20e6f065
David Ntim <dn123@dcc-login-03.oit.duke.edu>
2026-03-21 14:16:19 -0400

    adding mlp updates

---
+++ no_forget_molalkit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --time=48:00:00
+#SBATCH --partition=common
+
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+
+#SBATCH --export=ALL
+
+#SBATCH --job-name=Molalkit_dnn
+
+
+module load Anaconda3
+source activate molalkit
+
+# python molalkit_run --data_public bbb_martins --metrics roc_auc mcc accuracy precision recall f1_score --learning_type passive --model_config_selector David_MLP_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --save_dir bbb_david_mlp_mc
+
+python molalkit_run --data_public bace --metrics roc_auc mcc accuracy precision recall f1_score --learning_type passive --model_config_selector MLP_Morgan_BinaryClassification_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --save_dir bace_chemprop_no_forget
+
+# python molalkit_run --data_public bbb_martins --metrics roc_auc mcc accuracy precision recall f1_score --learning_type passive --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --save_dir bbb_rf
