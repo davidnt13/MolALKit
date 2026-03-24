be7bca7d3432e815ff3c9231a7ae03ebc3f9a22d
David Ntim <dn123@dcc-login-01.oit.duke.edu>
2026-03-24 11:41:07 -0400

    more pgp repeats

--- repeat_run_molalkit.sh
+++ repeat_run_molalkit.sh
@@ -17,17 +17,15 @@
 module load Anaconda3
 source activate molalkit
 
-# python molalkit_run --data_public pgp_broccatelli --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector David_MLP_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 1 --batch_size 2 --save_dir "test data/pgp/mlp/pgp_mlp_minf2" --forget_protocol MCDropoutForgetter --forget_size 200
-
-# python molalkit_run --data_public bbb_martins --metrics roc_auc mcc accuracy precision recall f1_score --learning_type passive --model_config_selector David_MLP_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --save_dir "test data/bbb_david_mforg_batch2_random" --forget_protocol MCDropoutForgetter --forget_size 250 --batch_size 2
-
-# python molalkit_run --data_public bace --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector MLP_Morgan_BinaryClassification_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --save_dir bace_dnn_mc_forget --forget_protocol MCDropoutForgetter --forget_size 2
-
 # python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 0 --batch_size 2 --save_dir "test data/pgp/rf/pgp_rf_explorative_minf2"  --forget_protocol min_oob_uncertainty --forget_size 200
 
-python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 1 --batch_size 2 --save_dir "test data/pgp/rf/pgp_rf_explorative_nf2"  #--forget_protocol min_oob_uncertainty --forget_size 200
+# python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 1 --batch_size 2 --save_dir "test data/pgp/rf/pgp_rf_explorative_nf2"  #--forget_protocol min_oob_uncertainty --forget_size 200
 
-python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 2 --batch_size 2 --save_dir "test data/pgp/rf/pgp_rf_explorative_nf3"  #--forget_protocol max_oob_uncertainty --forget_size 200
+# python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector RandomForest_Morgan_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 2 --batch_size 2 --save_dir "test data/pgp/rf/pgp_rf_explorative_nf3"  #--forget_protocol max_oob_uncertainty --forget_size 200
+
+python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector MLP_Morgan_BinaryClassification_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 1 --batch_size 2 --save_dir "test data/pgp/chemprop/pgp_chemprop_explorative_nf2"  #--forget_protocol min_oob_uncertainty --forget_size 200
+
+python molalkit_run --data_public pgp_broccatelli  --metrics roc_auc mcc accuracy precision recall f1_score --learning_type explorative --model_config_selector MLP_Morgan_BinaryClassification_Config --split_type scaffold_order --split_sizes 0.5 0.5 --evaluate_stride 10 --seed 2 --batch_size 2 --save_dir "test data/pgp/chemprop/pgp_chemprop_explorative_nf3"  #--forget_protocol max_oob_uncertainty --forget_size 200
 
 # Models Used:
 # Custom MLP: David_MLP_Morgan_Config
